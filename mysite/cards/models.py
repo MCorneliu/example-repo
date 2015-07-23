79540141633bcbf5488aad1f1d4ca706e029c4fd
Mihai-Corneliu Pojar <mihaicorneliu@gmail.com>
2015-07-23 13:30:02 +0300

    Added views

--- mysite/cards/models.py
+++ mysite/cards/models.py
@@ -1,9 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 
 class Card(models.Model):
 
+    owner = models.ForeignKey(User)
     url = models.URLField()
     name = models.CharField(max_length=60)
     image = models.ImageField()
